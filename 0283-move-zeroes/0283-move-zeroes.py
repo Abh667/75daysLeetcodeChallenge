13ebde593e54c39ed3a2b3194bbc8969a3e88086
Abhishek Kumar Mishra <abhishek123mishr@gmail.com>
2026-03-21 21:44:17 +0530

    Time: 0 ms (100%), Space: 13.5 MB (65.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0  # position for next non-zero
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+        
